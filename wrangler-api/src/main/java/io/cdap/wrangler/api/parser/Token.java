c2b26510355105b00cf76ff2daa497f1a0320edf
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-04-13 19:11:14 +0530

    Token.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/Token.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/Token.java
@@ -57,3 +57,126 @@ public interface Token extends Serializable {
    */
   JsonElement toJson();
 }
+package io.cdap.wrangler.api.parser;
+
+public enum TokenType {
+  STRING,
+  NUMBER,
+  BOOLEAN,
+  COLUMN_NAME,
+  BYTE_SIZE,
+  TIME_DURATION
+}
+package io.cdap.wrangler.api.parser;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.cdap.wrangler.api.Token;
+
+public class ByteSize implements Token {
+  private final long bytes;
+  private final String raw;
+
+  public ByteSize(String value) {
+    this.raw = value;
+    this.bytes = parseByteSize(value);
+  }
+
+  public long getBytes() {
+    return bytes;
+  }
+
+  @Override
+  public String value() {
+    return raw;
+  }
+
+  @Override
+  public TokenType type() {
+    return TokenType.BYTE_SIZE;
+  }
+
+  @Override
+  public JsonElement toJson() {
+    JsonObject json = new JsonObject();
+    json.addProperty("value", raw);
+    json.addProperty("bytes", bytes);
+    return json;
+  }
+
+  private long parseByteSize(String value) {
+    String normalized = value.toLowerCase();
+    double num = Double.parseDouble(normalized.replaceAll("[^0-9.]", ""));
+    String unit = normalized.replaceAll("[0-9.]", "");
+
+    switch (unit) {
+      case "b": return (long) num;
+      case "kb": return (long) (num * 1000);
+      case "kib": return (long) (num * 1024);
+      case "mb": return (long) (num * 1000 * 1000);
+      case "mib": return (long) (num * 1024 * 1024);
+      case "gb": return (long) (num * 1000 * 1000 * 1000);
+      case "gib": return (long) (num * 1024 * 1024 * 1024);
+      case "tb": return (long) (num * 1000 * 1000 * 1000 * 1000);
+      case "tib": return (long) (num * 1024 * 1024 * 1024 * 1024);
+      default: throw new IllegalArgumentException("Unknown byte unit: " + unit);
+    }
+  }
+}
+package io.cdap.wrangler.api.parser;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.cdap.wrangler.api.Token;
+
+public class TimeDuration implements Token {
+  private final long nanos;
+  private final String raw;
+
+  public TimeDuration(String value) {
+    this.raw = value;
+    this.nanos = parseTimeDuration(value);
+  }
+
+  public long getNanos() {
+    return nanos;
+  }
+
+  @Override
+  public String value() {
+    return raw;
+  }
+
+  @Override
+  public TokenType type() {
+    return TokenType.TIME_DURATION;
+  }
+
+  @Override
+  public JsonElement toJson() {
+    JsonObject json = new JsonObject();
+    json.addProperty("value", raw);
+    json.addProperty("nanos", nanos);
+    return json;
+  }
+
+  private long parseTimeDuration(String value) {
+    String normalized = value.toLowerCase();
+    double num = Double.parseDouble(normalized.replaceAll("[^0-9.]", ""));
+    String unit = normalized.replaceAll("[0-9.]", "");
+
+    switch (unit) {
+      case "ns": return (long) num;
+      case "us": return (long) (num * 1000);
+      case "ms": return (long) (num * 1000 * 1000);
+      case "s": return (long) (num * 1000 * 1000 * 1000);
+      case "m": return (long) (num * 60 * 1000 * 1000 * 1000);
+      case "h": return (long) (num * 3600 * 1000 * 1000 * 1000);
+      case "d": return (long) (num * 24 * 3600 * 1000 * 1000 * 1000);
+      default: throw new IllegalArgumentException("Unknown time unit: " + unit);
+    }
+  }
+}
+
+
+
